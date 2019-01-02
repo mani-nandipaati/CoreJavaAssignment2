6021814d1204ae271a2a7edf00dfde552ad1a1e9
mani <nandipaati.mani@gmail.com>
2019-01-02 21:38:11 +0530

    First Commit

---
+++ src/com/cts/serialization/service/SubjectService.java
@@ -0,0 +1,22 @@
+package com.cts.serialization.service;
+
+import com.cts.serialization.dao.SubjectDao;
+import com.cts.serialization.pojo.Subject;
+
+public class SubjectService {
+	
+	SubjectDao subjectDao = new SubjectDao();
+	
+	public void addSubject(Subject subject) {
+		subjectDao.addSubject(subject);
+	}
+	
+	public void deleteSubject(long id) {
+		subjectDao.deleteSubject(id);
+	}
+
+	public void searchSubject(long id) {
+		subjectDao.searchSubject(id);
+	}
+
+}
